aadc767acbb795492a068f3e36774470b810e00d
Pascal Schärli <pas.schaerli@sunrise.ch>
2019-05-02 23:08:08 +0200

    updated examples

--- example.py
+++ example.py
@@ -7,8 +7,8 @@ engine.say("hello world")
 
 engine = ResponsiveVoice(rate=0.5, vol=1)
 
-#engine.say("hello world", gender=ResponsiveVoice.MALE, lang=ResponsiveVoice.ENGLISH_US)
+engine.say("hello world", gender=ResponsiveVoice.MALE, lang=ResponsiveVoice.ENGLISH_US)
 
-file_path = engine.get_mp3("hello world", lang=ResponsiveVoice.ENGLISH_US)
+file_path = engine.get_mp3(u"hola mundo", lang=ResponsiveVoice.PORTUGESE_PT)
 
 engine.play_mp3(file_path)
