c1f1fdaa86774b84ee69a39d7adbc101a04cafc0
Pascal Schärli <pas.schaerli@sunrise.ch>
2019-05-02 23:05:38 +0200

    Fixes

--- example.py
+++ example.py
@@ -1,8 +1,14 @@
+#!/usr/bin/python3
+
 from responsive_voice import ResponsiveVoice
 
 engine = ResponsiveVoice()
 engine.say("hello world")
-engine.say("hello world", gender="male", lang="en-GB", rate=0.4)
 
-file_path = engine.get_mp3(u"ola mundo", lang="pt-pt")
+engine = ResponsiveVoice(rate=0.5, vol=1)
+
+#engine.say("hello world", gender=ResponsiveVoice.MALE, lang=ResponsiveVoice.ENGLISH_US)
+
+file_path = engine.get_mp3("hello world", lang=ResponsiveVoice.ENGLISH_US)
+
 engine.play_mp3(file_path)
--- responsive_voice/__init__.py
+++ responsive_voice/__init__.py
@@ -49,7 +49,7 @@ class ResponsiveVoice(object):
     GREEK = "el-GR"
     JAPANESE = "ja-JP"
 
-    def __init__(self, lang=self.ENGLISH_US, pitch=0.5, rate=0.5, vol=1, gender=self.FEMALE):
+    def __init__(self, lang=ENGLISH_US, pitch=0.5, rate=0.5, vol=1, gender=FEMALE):
         self.pitch = pitch
         self.rate = rate
         self.vol = vol
@@ -60,12 +60,15 @@ class ResponsiveVoice(object):
         else:
             self.vn = self.sv = ""
 
-    def play_mp3(self, mp3_file, play_cmd="mpg123 -q %1"):
+    def play_mp3(self, mp3_file, play_cmd="mpg123 -q %1", blocking=False):
         play_mp3_cmd = str(play_cmd).split(" ")
         for index, cmd in enumerate(play_mp3_cmd):
             if cmd == "%1":
                 play_mp3_cmd[index] = (mp3_file)
-        return subprocess.Popen(play_mp3_cmd)
+        if blocking:
+            return subprocess.call(play_mp3_cmd)
+        else:
+            return subprocess.Popen(play_mp3_cmd)
 
     def get_mp3(self, sentence, mp3_file=None, lang=None, pitch=None, rate=None, vol=None, gender=None):
         if mp3_file is None:
@@ -88,12 +91,19 @@ class ResponsiveVoice(object):
 
         base_url = "https://code.responsivevoice.org/getvoice.php"
         r = requests.get(base_url, params)
-        with open(mp3_file, "w") as f:
+
+        with open(mp3_file, "wb") as f:
             f.write(r.content)
         return mp3_file
 
     def say(self, sentence, mp3_file=None, lang=None, pitch=None, rate=None, vol=None, gender=None, play_cmd="mpg123 -q %1"):
-        filename = "tmp_" + ''.join(random.choice(string.ascii_letters) for _ in range(10)) + ".mp3"
-        self.get_mp3(sentence, filename, lang=lang, pitch=pitch, rate=rate, vol=vol, gender=gender)
-        self.play_mp3(filename, play_cmd)
-        os.remove(filename)
+        if mp3_file is None:
+            filename = "tmp_" + ''.join(random.choice(string.ascii_letters) for _ in range(10)) + ".mp3"
+            blocking = True
+        else:
+            filename = mp3_file
+            blocking = False
+        filename = self.get_mp3(sentence, filename, lang=lang, pitch=pitch, rate=rate, vol=vol, gender=gender)
+        self.play_mp3(filename, play_cmd, blocking=blocking)
+        if mp3_file is None:
+            os.remove(filename)
