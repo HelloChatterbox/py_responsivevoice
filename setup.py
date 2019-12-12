091fab76920c624bd5b8a7b69d8f1f7e4e9ec5a2
jarbasal <jarbasai@mailfence.com>
2019-12-12 22:22:07 +0000

    add changelog + license

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 
 setup(
     name='ResponsiveVoice',
-    version='0.4.0',
+    version='0.4.1',
     packages=['responsive_voice'],
     url='https://github.com/JarbasAl/py_responsivevoice',
     install_requires=["requests"],
-    license='MIT',
+    license='Apache2',
     author='jarbasAI',
     author_email='jarbasai@mailfence.com',
     description='unofficial python wrapper for responsive voice'
